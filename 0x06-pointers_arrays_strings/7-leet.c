f66fa6672af55535d4a3643f559c27f1042b027b
konchellah bramel <kontunta143@gmail.com>
2023-03-31 07:52:49 -0700

    print buffer

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+int i, j;
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
+for (i = 0 ; str[i] != '\0'; i++)
+{
+for (j = 0 ; a[j] != '\0'; j++)
+{
+if (str[i] == a[j])
+str[i] = b[j]
+}
+}
+return(n);
+}
