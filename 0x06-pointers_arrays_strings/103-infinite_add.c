7a1dd0695d34e55a3e5890e201216606324c7c39
konchellah bramel <kontunta143@gmail.com>
2023-04-01 06:58:12 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,24 +1,60 @@
 #include "main.h"
 
-/*
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
+/**
+ * infinite_add - adds two numbers
+ * @n1: the first number to add
+ * @n2: the second number to add
+ * @r: the buffer to store the result
+ * @size_r: the buffer size
+ *
+ * Return: a pointer to the result, or 0 if the result cannot be stored in r
  */
-void rev_string(char *n)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i = 0;
-int j = 0;
-char temp ;
+int len1 = 0, len2 = 0, i, j, sum, carry = 0;
 
-while (*(n + i) != '\0')
+while (n1[len1] != '\0')
+len1++;
+while (n2[len2] != '\0')
+len2++;
+
+if (len1 >= size_r || len2 >= size_r)
+return (0);
+
+i = len1 - 1;
+j = len2 - 1;
+r[size_r - 1] = '\0';
+
+while (i >= 0 || j >= 0)
 {
-i++;
-}
+sum = carry;
+if (i >= 0)
+sum += n1[i] - '0';
+if (j >= 0)
+sum += n2[j] - '0';
+
+if (sum > 9)
+carry = 1;
+else
+carry = 0;
+
+r[size_r - 2] = sum % 10 + '0';
+size_r--;
 i--;
+j--;
+}
 
-for (j = 0; j < i; j++, i--)
+if (carry == 1)
 {
-temp = *(n + j);
+if (size_r == 1)
+return (0);
+r[size_r - 2] = '1';
+}
+else
+{
+r[size_r - 2] = '0';
+}
 
+return (r + size_r - 1 - len1 - len2);
+}
 
