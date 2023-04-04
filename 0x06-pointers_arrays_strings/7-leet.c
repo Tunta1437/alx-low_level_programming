5a5348ccf0200fa16bb373e701c519596d6996c4
konchellah bramel <kontunta143@gmail.com>
2023-04-04 08:08:44 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,30 @@
-char *leet(char *str)
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
+ */
+
+void print_diagonal(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
 {
 int i, j;
-char *leet_str = str;
 
-char leet_chars[5][2] = {
-{'a', '4'},
-{'e', '3'},
-{'o', '0'},
-{'t', '7'},
-{'l', '1'}
-};
-
-for (i = 0; str[i] != '\0'; i++)
+for (i = 0; i < n; i++)
 {
-for (j = 0; j < 5; j++) 
+for (j = 0; j < n; j++)
 {
-
-if (str[i] == leet_chars[j][0] || str[i] == leet_chars[j][0] - 32  
-{
-leet_str[i] = leet_chars[j][1];
+if (j == i)
+_putchar('\\');
+else if (j < i)
+_putchar(' ');
+}
+_putchar('\n');
 }
 }
 }
-
-return leet_str;
-}
-
