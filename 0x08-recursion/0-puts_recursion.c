cc1b572ef7be9d5caafcdafd3f1503718d4a137b
konchellah bramel <kontunta143@gmail.com>
2023-04-05 07:07:16 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,28 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /*
- *base case: end of string
- *_puts_recursion - make a recursive call with the next character
- **s: string to be copied
+ *_puts_recursion - function like puts();
+ **s: input
  *
  *Return: 0
  */
 void _puts_recursion(char *s);
 {
-if (*s == '\0')
-{
-putchar ('\n');
-return (0);
-}
+if (*s)
 
-putchar(*s);
+_putchar(*s);
 _puts_recursion(s + 1);
-{
-int main(void);
-}
-char str[] = "Hello, world!";
-_puts_recursion(str);
-return (0);
+
+else
+putchar ('\n');
 }
 
